487896640f518eef55233dfd2e790ce5c137645d
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2022-10-19 19:13:27 +0530

    init

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,8 +3,8 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
-      <Button />
+      <h1>Menu.saklani.dev</h1>
+      {/* <Button /> */}
     </div>
   );
 }
