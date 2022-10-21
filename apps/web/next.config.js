c1bf7142f08d3fac0e8677c8566365b974be64b3
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2022-10-21 12:03:45 +0530

    feat: boilerplate added

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,22 @@
-const withTM = require("next-transpile-modules")(["ui"]);
+const withBundleAnalyzer = require('@next/bundle-analyzer')
+const withTM = require('next-transpile-modules')
 
-module.exports = withTM({
-  reactStrictMode: true,
-});
+/**
+ * @type {import('next').NextConfig}
+ */
+const config = {
+  reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    formats: ['image/avif', 'image/webp']
+  },
+  experimental: {}
+}
+
+module.exports = (_phase, { defaultConfig: _ }) => {
+  const plugins = [
+    withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' }),
+    withTM([]) // add modules you want to transpile here
+  ]
+  return plugins.reduce((acc, plugin) => plugin(acc), { ...config })
+}
---
+++ apps/web/src/pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript
+} from 'next/document'
+import * as React from 'react'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body style={{ opacity: 0 }}>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
