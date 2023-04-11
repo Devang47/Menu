4c23a0196d0ddc6d93b8c776098b036454d21fe0
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2023-04-11 14:22:10 +0530

    bg image compressed

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -6,6 +6,6 @@
 
 <style lang="postcss">
 	:global(html) {
-		background-image: url('https://cdn.raster.app/kitatsu/library-1/oScZSUoQVJ?ixlib=js-3.7.0&width=2813&height=3640&compress=false&blur=374&bri=-8&s=bc8218ec5c490a2477258b0be93b0921');
+		background-image: url('https://cdn.raster.app/kitatsu/library-1/oScZSUoQVJ?ixlib=js-3.7.0&width=2813&height=3640&compress=false&blur=374&bri=-8&fm=webp&q=30&s=5ee3f94d8f06ab702cf40220c12ffbd4');
 	}
 </style>
